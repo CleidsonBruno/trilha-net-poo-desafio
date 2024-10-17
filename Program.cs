9c045c7b4b9bc4c61651d57ade2a457d825efcc0
cleidson bruno <cleidsonhigio@hotmail.com>
2024-10-17 09:46:18 -0300

    concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "1111111111", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Smartphone iphone = new Iphone("987654321", "iPhone 13", "2222222222", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
